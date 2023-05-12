11950339986da4790a1a8cfae95d1f74efecc9a0
Ewin <emathew@ucsc.edu>
2023-05-12 13:45:08 -0700

    assets added; methods created; 3 scenes finished

--- game.js
+++ game.js
@@ -1,131 +1,184 @@
-class Demo1 extends AdventureScene {
-    constructor() {
-        super("demo1", "First Room");
-    }
-
-    onEnter() {
-
-        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => this.showMessage("Metal, bent."))
-            .on('pointerdown', () => {
-                this.showMessage("No touching!");
-                this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
-                });
-            });
-
-        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("It's a nice key.")
-            })
-            .on('pointerdown', () => {
-                this.showMessage("You pick up the key.");
-                this.gainItem('key');
-                this.tweens.add({
-                    targets: key,
-                    y: `-=${2 * this.s}`,
-                    alpha: { from: 1, to: 0 },
-                    duration: 500,
-                    onComplete: () => key.destroy()
-                });
-            })
-
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
-                }
-            })
-            .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
-                }
-            })
-
-    }
-}
-
-class Demo2 extends AdventureScene {
-    constructor() {
-        super("demo2", "The second room has a long name (it truly does).");
-    }
-    onEnter() {
-        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("You've got no other choice, really.");
-            })
-            .on('pointerdown', () => {
-                this.gotoScene('demo1');
-            });
-
-        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage('*giggles*');
-                this.tweens.add({
-                    targets: finish,
-                    x: this.s + (this.h - 2 * this.s) * Math.random(),
-                    y: this.s + (this.h - 2 * this.s) * Math.random(),
-                    ease: 'Sine.inOut',
-                    duration: 500
-                });
-            })
-            .on('pointerdown', () => this.gotoScene('outro'));
-    }
-}
-
 class Intro extends Phaser.Scene {
     constructor() {
-        super('intro')
+        super('intro');
     }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
-        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
+        this.add.text(50, 50, "\n\nClick anywhere to begin.").setFontSize(40);
         this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+            this.cameras.main.fade(1000, 0, 0, 0);
+            this.time.delayedCall(1000, () => this.scene.start('introcut'));
         });
     }
 }
 
-class Outro extends Phaser.Scene {
+class IntroCut extends Phaser.Scene {
     constructor() {
-        super('outro');
+        super('introcut');
+    }
+    preload() {
+        this.load.path = './assets/';
+        this.load.image('bg', 'background.png');
+        this.load.image('chef', 'chef.png');
+        this.load.image('counter', 'counter.png');
+        this.load.image('burger', 'bruger.png');
     }
     create() {
-        this.add.text(50, 50, "That's all!").setFontSize(50);
-        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
-        this.input.on('pointerdown', () => this.scene.start('intro'));
+        let bg = this.add.image(960, 540, 'bg');
+        bg.setScale(2.7);
+        let chef = this.add.image(960, 540, 'chef');
+        chef.setScale(2.7);
+        let counter = this.add.image(960, 540, 'counter');
+        counter.setScale(2.7);
+        let burger = this.add.image(1400, -600, 'burger'); // drops to 600
+        burger.setScale(1.2);
+        let progress_text = this.add.text(1150, 300, 'Click to continue', {fontSize: '50px', color: '#000000'});
+        progress_text.alpha = 0;
+        this.tweens.chain({
+            tweens: [
+                {targets: burger, y: 600, ease: 'Bounce.easeOut', duration: 1500},
+                {targets: chef, x: 3000, duration: 3000},
+                {targets: burger, scaleX: -1, duration: 100},
+                {targets: chef, x: 3001, duration: 1000},
+                {targets: burger, scaleX: 1, duration: 100},
+                {targets: chef, x: 3000, duration: 1000},
+                {targets: burger, x: -200, ease: 'Linear', duration: 700},
+                {targets: progress_text, alpha: 1, duration: 500}
+            ]
+        });
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0, 0, 0);
+            this.time.delayedCall(1000, () => this.scene.start('room1'));
+        });
     }
 }
 
+class Kitchen extends AdventureScene {
+    constructor() {
+        super('room1', 'Kitchen');
+    }
+    preload() {
+        this.load.path = './assets/';
+        this.load.image('table', 'table.png');
+        this.load.image('burger', 'bruger.png');
+        this.load.image('counter_td', 'counter_topdown.png');
+        this.load.image('backdoor', 'backdoor.png');
+        this.load.image('doomdoor', 'doomdoor.png');
+    }
+    onEnter() {
+        let table_check = 2;
+        let counter_td = this.add.image(400, 600, 'counter_td')
+            .setScale(-1.5)
+        let table = this.add.image(190, 250, 'table')
+            .setScale(-.7)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("There seems to be some loot");
+            })
+            .on('pointerdown', () => {
+                if (table_check == 2) {this.showMessage("You find a cheeseburger-sized hammer");
+                this.gainItem('cheeseburger-sized hammer'); table_check--;}
+                else if (table_check == 1) {this.showMessage("You find a stale french fry");
+                this.gainItem('stale french fry'); table_check--; table.removeInteractive();}
+                ;
+            });
+        let backdoor = this.add.image(1200, 150, 'backdoor')
+            .setScale(.7)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("It seems to be the way out");
+            })
+            .on('pointerdown', () => {
+                if (this.hasItem('cheeseburger-sized hammer')) {
+                    this.showMessage('You break your way out');
+                    this.loseItem('cheeseburger-sized hammer');
+                    this.gotoScene('room2');
+                } else {
+                    this.showMessage("The door won't budge");
+                }
+            });
+        let check = 1;
+        let doomdoor = this.add.image(300, 900, 'doomdoor')
+            .setScale(.9)
+            .setInteractive()
+            .on('pointerover', () => {
+                if (check == 1) {this.ominous(doomdoor); check--};
+            })
+            .on('pointerdown', () => {
+                this.showMessage('There is something ominous about that door, it might be better to not enter')
+            })
+        let burger = this.add.image(700, 570, 'burger')
+            .setScale(.3)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("That's you");
+            });
+    }
+}
+
+class BackAlley extends AdventureScene {
+    constructor() {
+        super('room2', 'Back Alley');
+    }
+    preload() {
+        this.load.path = './assets/';
+        this.load.image('burger', 'bruger.png');
+        this.load.image('vanquisher', 'vanquisher.png');
+        this.load.image('arrow', 'arrow.png');
+        this.load.image('yes', 'yes.png');
+        this.load.image('no', 'no.png');
+    }
+    onEnter() {
+        let check = 1;
+        let vanquisher = this.add.image(700, 400, 'vanquisher')
+            .setScale(.7)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('This beast is truly the gluten gobbler')
+                if (check == 1) {this.ominous(vanquisher); check--};
+            })
+            .on('pointerdown', () => {
+                this.unwinnablefight(700, 400, vanquisher);
+                vanquisher.removeInteractive();
+            });
+        let left = this.add.image(150, 600, 'arrow')
+            .setScale(.5)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('One of 2 ways you can go. This path leads to a dangerous looking street');
+            })
+            .on('pointerdown', () => {
+                this.gotoScene('room3a');
+            });
+        let right = this.add.image(1300, 600, 'arrow')
+            .setScale(-.5)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('One of 2 ways you can go. This path leads to a dangerous looking forest path');
+            })
+            .on('pointerdown', () => {
+                this.gotoScene('room3b');
+            });
+    }
+}
+
+class DangerousStreet extends AdventureScene {
+    constructor() {
+        super('room3a', 'Dangerous Street');
+    }
+    preload() {
+        this.load.path = './assets/';
+        this.load.image('burger', 'bruger.png');
+    }
+}
 
 const game = new Phaser.Game({
     scale: {
+        type: Phaser.WEBGL,
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
-        height: 1080
+        height: 1080,
     },
-    scene: [Intro, Demo1, Demo2, Outro],
-    title: "Adventure Game",
-});
-
+    scene: [/*Intro, IntroCut,*/ Kitchen, BackAlley],
+    title: "burgventure"
+})
